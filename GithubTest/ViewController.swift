35f8d9511346105808957391199c741acd8b242e
Dhana Gadupooti <dhanag@ktechproduts.com>
2021-06-07 14:50:43 +0530

    text change

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.text = "welcome"
     }
 
 
